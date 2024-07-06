df50ce4f2b8affb466e35d603e6921888f940e9b
yusuShin <tlsdbtn0507@naver.com>
2024-07-06 21:25:26 +0900

    워크플로 에러 수정 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
