60fab187d5efc4887dc4e7a82fcc058c1517a559
yusuShin <tlsdbtn0507@naver.com>
2024-07-12 22:10:59 +0900

    live 연결 테스트 4

--- src/components/sign/idCheckBtn.tsx
+++ src/components/sign/idCheckBtn.tsx
@@ -11,7 +11,7 @@ const IdCheckBtn = (props: BtnType) => {
     e.preventDefault();
     props.onClick(e);
     setIsClicked(true);
-    console.log(process.env.REACT_APP_SERVER,process.env.NODE_ENV);
+    console.log(process.env.REACT_APP_SERVER,process.env.NODE_ENV,1);
   };
   
   let btnContent = "아이디 중복 조회";
