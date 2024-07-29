a784a714e1ab68febaca6244b6f978b341e39054
yusuShin <tlsdbtn0507@naver.com>
2024-07-29 21:36:00 +0900

    api 인터셉터 추가 중

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios"
 
 const API = axios.create({
   baseURL: process.env.REACT_APP_SERVER,
@@ -7,6 +7,21 @@ const API = axios.create({
     'Content-Type': 'application/json',
   }
 })
+
+API.interceptors.response.use((response:AxiosResponse) => response,
+  async (error:AxiosError) => {
+    const original = error.config as AxiosRequestConfig & { _retry?: boolean };
+    if (error.response?.status === 401 && !original._retry ) {
+      original._retry = true;
+      try {
+        
+      } catch (error) {
+        
+      }
+    }
+
+  }
+)
 
 export default API
 
