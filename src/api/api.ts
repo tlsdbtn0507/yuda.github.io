85eccca9eff89fe0f38175d9a2088e323fe2b8f8
yusuShin <tlsdbtn0507@naver.com>
2024-07-30 23:28:31 +0900

    api 인터셉터 완성

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,25 +1,35 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios"
+import { tokenSet } from "utils/util";
+import { renewToken } from "./users/usersApi";
 
 const API = axios.create({
   baseURL: process.env.REACT_APP_SERVER,
   withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  }
+  // headers: {
+  //   'Content-Type': 'application/json',
+  // }
 })
 
 API.interceptors.response.use((response:AxiosResponse) => response,
   async (error:AxiosError) => {
-    const original = error.config as AxiosRequestConfig & { _retry?: boolean };
-    if (error.response?.status === 401 && !original._retry ) {
-      original._retry = true;
+    const originalRequest = error.config as AxiosRequestConfig & { _retry?: boolean };
+    if (error.response?.status === 401 && !originalRequest._retry ) {
+      originalRequest._retry = true;
       try {
-        
+        const { accessToken } = await renewToken() as { accessToken: string };
+        console.log('renewed')
+        alert('renewed')
+        axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+
+        if (!originalRequest.headers) originalRequest.headers = {};
+
+        originalRequest.headers['Authorization'] = `Bearer ${accessToken}`;
+        return axios(originalRequest);
       } catch (error) {
-        
+        console.error('토큰 갱신 실패:', error);
       }
     }
-
+    return Promise.reject(error);
   }
 )
 
