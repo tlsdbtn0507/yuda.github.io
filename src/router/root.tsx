240b41d3fdbc375ad88531766b6f294d2f0ed055
yusuShin <tlsdbtn0507@naver.com>
2024-08-22 23:28:24 +0900

    일기 작성 컴포넌트화 및 닫기버튼 분리로 인한 코드정리

--- src/router/root.tsx
+++ src/router/root.tsx
@@ -1,11 +1,10 @@
-import { useEffect } from 'react';
 import Intro from '../components/intro';
 import css from '../css/app.module.css';
-import { Outlet, useLocation, useNavigation } from 'react-router';
+
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
 import { diaryStore } from '../store/diary/diaryStore';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const Root = () =>{
   const { pathname } = useLocation();
@@ -21,11 +20,6 @@ const Root = () =>{
     setScreenSize();
   });
 
-  const closeBtn = writeDairy &&
-          <button className={css.closeBtn} >
-            <FontAwesomeIcon className={css.closeBtnIcon} icon={faXmark}/>
-          </button>
-
   return (
     <div className={css.app}>
       <Link className={css.h1} to={"/"}>
@@ -35,7 +29,6 @@ const Root = () =>{
           }
         </p>
       </Link>
-        {closeBtn}
       <main>
         <Outlet/>
       </main>
