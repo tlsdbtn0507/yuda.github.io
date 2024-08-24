7965f9197fc676118255e4eb216b0290ac6b8da0
yusuShin <tlsdbtn0507@naver.com>
2024-08-25 00:16:57 +0900

    일기 문구 애니메이션 작업

--- src/router/root.tsx
+++ src/router/root.tsx
@@ -14,37 +14,34 @@ const Root = () =>{
   const [displayText, setDisplayText] = useState('YuDa');
 
 
-  const setScreenSize = ()=> {
+  const setScreenSize = () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
-  }
+  };
+
+  const aniSetter = (str:string) => {
+    setAnimating(true);
+     // 애니메이션이 끝난 후에 writeDairy 상태 변경
+    setTimeout(() => {
+      setDisplayText(str);
+      setAnimating(false);
+    }, 800);     
+  };
+
+  const animaCssClass =
+    `${writeDairy ? css.ment : css.yuda} ${animating ? css.animating : ''}`;
+  
+  const linkCssClass = writeDairy ? css.h1 : css.h2
   
   useEffect(() => {
-    if (writeDairy) {
-      setAnimating(true);
-      // 애니메이션이 끝난 후에 writeDairy 상태 변경
-      setTimeout(() => {
-        setDisplayText('오늘의 일기를 작성해 봐요');
-        setAnimating(false);
-      }, 800); 
-    } else {
-      setAnimating(true);
-      setTimeout(() => {
-        setDisplayText('YuDa');
-        setAnimating(false);
-      }, 800); 
-    }
+    writeDairy ? aniSetter('오늘의 일기를 작성해 봐요') : aniSetter('YuDa');
     setScreenSize()
   },[writeDairy]);
 
   return (
     <div className={css.app}>
-      <Link className={css.h1} to={"/"}>
-        <p className=
-          {`${writeDairy ? css.yuda20 : css.yuda} ${animating ? css.animating : ''}`}>
-          {/* { writeDairy ? '오늘의 일기를 작성해 봐요' : 'YuDa'} */}
-          {displayText}
-        </p>
+      <Link className={linkCssClass} to={"/"}>
+        <p className={animaCssClass}> {displayText}</p>
       </Link>
       <main>
         <Outlet/>
