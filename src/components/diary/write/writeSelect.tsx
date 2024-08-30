215275ae6b22cdf1645aabff4f91653071b5b00e
yusuShin <tlsdbtn0507@naver.com>
2024-08-30 20:25:33 +0900

    감정과 에니메이션 추가

--- src/components/diary/write/writeSelect.tsx
+++ src/components/diary/write/writeSelect.tsx
@@ -1,10 +1,37 @@
+import React, { useEffect, useState } from 'react';
 import css from '../../../css/write.module.css'
+import { WriteDiaryFeelings } from 'model/types';
 
-const WriteSelect = () => {
+interface WriteSelectProps{
+  type: string,
+  selections: WriteDiaryFeelings
+}
+
+const WriteSelect:React.FC<WriteSelectProps> = ({type,selections}):React.ReactElement => {
+
+  const [renderAnima, setRenderAnima] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setRenderAnima(true);
+    }, 1500);
+  }, [renderAnima]);
+
+  let h2;
+
+  switch (type) {
+    case 'feeling':
+      h2 = '오늘 하루는 어떠셨나요?';
+      break;
+  
+    default:
+      break;
+  }
 
   return (
     <div className={css.selectWrapper}>
-      <h2 className={css.sectionMent}>{ '오늘 하루는 어떠셨나요?' }</h2>
+      <h2 className={renderAnima ? css.sectionMentS : css.sectionMent}>{h2}</h2>
+      {`${renderAnima},${selections}`}
     </div>
   )
 };
