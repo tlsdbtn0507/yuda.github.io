695ee4a7946755e5686186d0e50c79c21f0e4fba
yusuShin <tlsdbtn0507@naver.com>
2024-09-02 19:30:26 +0900

    일기 감정 버튼 및 로직 화면 개발

--- src/components/diary/write/write.tsx
+++ src/components/diary/write/write.tsx
@@ -1,12 +1,46 @@
 import CloseBtn from 'components/util/closeBtn';
 import css from '../../../css/write.module.css'
 import WriteSelect from './writeSelect';
+import { useEffect, useState } from 'react';
+import { FEELINGS, WriteDiary, WriteDiaryFeeling } from 'model/types';
+import { isEmptyObj } from 'utils/util';
 
 const Write = () => {
+
+  const [content, setContent] = useState<React.ReactElement | null>(null);
+
+  const whichSelectRender = (which: string) => {
+    switch (which) {
+      case 'f':
+        const feelings = FEELINGS as WriteDiaryFeeling[];
+        setContent(<WriteSelect type={'feeling'} selections={feelings} />);
+        break;
+    
+      default:
+        break;
+    }
+  }
+
+  useEffect(() => { 
+    const isDiaryWritten = localStorage.getItem('writingDiary');
+
+    if (!isDiaryWritten) {
+      const writingDiary = new Object({ feeling: {} });
+      localStorage.setItem('writingDiary', JSON.stringify(writingDiary));
+      return whichSelectRender('f')
+    };
+    
+    const writingDiary = JSON.parse(isDiaryWritten as string) as WriteDiary;
+    
+    if (isEmptyObj(writingDiary.feeling)) whichSelectRender('f')
+
+  }, []);
+
+
   return (
     <div className={css.total}>
       <CloseBtn />
-      <WriteSelect/>
+      {content}
     </div>
   )
 };
--- src/components/diary/write/writeSelect.tsx
+++ src/components/diary/write/writeSelect.tsx
@@ -1,37 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import css from '../../../css/write.module.css'
-import { WriteDiaryFeelings } from 'model/types';
+import { WriteDiaryFeeling } from 'model/types';
+import WriteButton from './writeButton';
 
 interface WriteSelectProps{
   type: string,
-  selections: WriteDiaryFeelings
+  selections: WriteDiaryFeeling[]
 }
 
 const WriteSelect:React.FC<WriteSelectProps> = ({type,selections}):React.ReactElement => {
 
   const [renderAnima, setRenderAnima] = useState<boolean>(false);
 
+  let h2;
+
   useEffect(() => {
     setTimeout(() => {
       setRenderAnima(true);
     }, 1500);
   }, [renderAnima]);
-
-  let h2;
-
+    
   switch (type) {
     case 'feeling':
       h2 = '오늘 하루는 어떠셨나요?';
       break;
-  
+    
     default:
       break;
-  }
+  };
 
   return (
     <div className={css.selectWrapper}>
       <h2 className={renderAnima ? css.sectionMentS : css.sectionMent}>{h2}</h2>
-      {`${renderAnima},${selections}`}
+      <div className={renderAnima ? css.selectBtnWrapS : css.selectBtnWrap}>
+        {selections.map(e => <WriteButton key={e.level} ment={e.ment} />)}
+      </div>
     </div>
   )
 };
