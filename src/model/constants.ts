42ec6889fa85d11b152b8ebdc00a4b70f0e0623e
yusuShin <tlsdbtn0507@naver.com>
2024-09-04 23:33:49 +0900

    상수 파일 추가

---
+++ src/model/constants.ts
@@ -0,0 +1,16 @@
+import { WriteDiaryFeeling } from "./interfaces";
+
+export const FEELINGS:WriteDiaryFeeling[] = [
+    {
+      ment: '완전 행복!',
+      level:3
+    },
+    {
+      ment: '그냥 그랬어',
+      level:2
+    },
+    {
+      ment: '별로..',
+      level:1
+    },
+]
