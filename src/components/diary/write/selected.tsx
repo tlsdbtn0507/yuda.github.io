2d86c7607528190020c1d71a89aeccfd0dccf952
yusuShin <tlsdbtn0507@naver.com>
2024-09-08 23:21:00 +0900

    selected 하위 컴포넌트 remind

--- src/components/diary/write/selected.tsx
+++ src/components/diary/write/selected.tsx
@@ -1,16 +1,24 @@
 import css from '../../../css/write.module.css'
 import { diaryStore } from 'store/diary/diaryStore'
 import { isEmptyObj } from 'utils/util';
+import RemindBtn from './remindBtn';
+import { WriteDiaryFeeling, WriteDiaryWeather } from 'model/interfaces';
 
 const Selected = () => {
 
   const { isDiaryWritten } = diaryStore(state => state);
 
+  const isBtnOpacity = (toCheck: WriteDiaryFeeling | WriteDiaryWeather) =>
+    !isEmptyObj(toCheck);
+
   return (
     <div className={css.selectedWrapper}>
-      {isEmptyObj(isDiaryWritten.feeling) ? '' : 
-        <p>asd</p>
-      }
+      <div style={{opacity:+isBtnOpacity(isDiaryWritten.feeling as WriteDiaryFeeling)}}>
+        <RemindBtn type={isDiaryWritten.feeling as WriteDiaryFeeling} />
+      </div>
+      <div style={{opacity:+isBtnOpacity(isDiaryWritten.weather as WriteDiaryFeeling)}}>
+        <RemindBtn type={isDiaryWritten.weather as WriteDiaryFeeling} />
+      </div>
     </div>
   )
 }
