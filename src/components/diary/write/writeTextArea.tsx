61d1e57881903b5c66e1e0c9f8ba82e2d7b59a3d
yusuShin <tlsdbtn0507@naver.com>
2024-09-16 19:49:27 +0900

    일기 감정문 전역 저장

--- src/components/diary/write/writeTextArea.tsx
+++ src/components/diary/write/writeTextArea.tsx
@@ -1,11 +1,23 @@
+import { useRef } from 'react';
 import css from '../../../css/write.module.css'
+import { diaryStore } from 'store/diary/diaryStore';
 
 const WriteTextArea = () => {
+
+  const textRef = useRef<HTMLTextAreaElement>(null);
+  const { setWritingDiary , isDiaryWritten } = diaryStore(state => state);
   
+  const writeDairyFeelingReason = () => {
+    const feelingReason = textRef.current!.value;
+    const writing = { ...isDiaryWritten, feelingReason };
+    setWritingDiary(writing);
+  };
 
   return (
-    <textarea className={css.textArea} placeholder='직접 적어보아요'>
-
+    <textarea
+      ref={textRef}
+      className={css.textArea} placeholder='직접 적어보아요'
+      onBlur={writeDairyFeelingReason}>
     </textarea>
   )
 }
