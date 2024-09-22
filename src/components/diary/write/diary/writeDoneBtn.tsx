1a8c3863191476e2279d35563e1c46a0d0a76bbe
yusuShin <tlsdbtn0507@naver.com>
2024-09-22 21:07:46 +0900

    일기 감정문 작성완료 버튼 컴포넌트 제작

--- src/components/diary/write/diary/writeDoneBtn.tsx
+++ src/components/diary/write/diary/writeDoneBtn.tsx
@@ -1,10 +1,38 @@
+import { diaryStore } from 'store/diary/diaryStore'
 import css from '../../../../css/write.module.css'
+import { whichObjIsEmpty } from 'utils/util';
 
 const WriteDoneBtn = () => {
 
+  const { isDiaryWritten } = diaryStore(state => state);
+
+  const isBtnDisabled = isDiaryWritten.feelingReason?.length === 0;
+
+  const handleDoneBtn = () => {
+    switch (isBtnDisabled) {
+      case true:
+        const checkReasonNull = window.confirm('정말 아무것도 안적으시게요?');
+        if (checkReasonNull) return console.log('go all selected');
+        break;
+      
+      case false:
+        console.log('go all selected');
+        console.log(whichObjIsEmpty(isDiaryWritten));
+        break;
+    
+      default:
+        break;
+    }
+
+  };
+
+  const doneBtnClass = isBtnDisabled ? css.notDoneBtn : css.doneBtn
+
   return (
-    <button className={css.doneBtn}>
-      작성완료!
+    <button
+      className={doneBtnClass}
+      onClick={handleDoneBtn}>
+      작성완료{isBtnDisabled ? '?' :'!'}
     </button>
   )
 }
