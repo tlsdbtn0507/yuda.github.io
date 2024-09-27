0108758a264470674a16cbfc798381e97c5a824a
yusuShin <tlsdbtn0507@naver.com>
2024-09-27 22:05:37 +0900

    remindBtn, selected 폐기 컴포넌트 재활용

--- src/components/diary/write/summary/writeSum.tsx
+++ src/components/diary/write/summary/writeSum.tsx
@@ -1,10 +1,12 @@
 import css from '../../../../css/write.module.css'
+import Selected from './selected'
 
 const WriteSum = () => {
 
   return (
     <div className={css.writeSumWrapper}>
       <h2>오늘 하루는</h2>
+      <Selected />
     </div>
   )
 }
