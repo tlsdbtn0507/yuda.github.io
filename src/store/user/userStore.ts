040f25e0174ecc6b97832c86fe15cd726b6ba7a2
yusuShin <tlsdbtn0507@naver.com>
2024-10-03 22:10:57 +0900

    현재 위치 좌표 호출함수 저장

--- src/store/user/userStore.ts
+++ src/store/user/userStore.ts
@@ -1,9 +1,12 @@
+import { getLocation } from "api/api";
 import { create } from "zustand";
 
 interface UserState  {
   pw: string,
   pwCheck: boolean,
-  accessToken:string,
+  accessToken: string,
+  currentLoc: { lat: string, long: string },
+  setCurrentLoc: () => void,
   setUserPw: (pw: string) => void,
   setUserPWCheck : (check:boolean) =>void
 }
@@ -11,7 +14,12 @@ interface UserState  {
 export const userStore = create<UserState>((set) => ({
   pw: '',
   pwCheck: false,
-  accessToken:'',
+  accessToken: '',
+  currentLoc: { long: '', lat: '' },
+  setCurrentLoc: () => async () => {
+    const [lat,long] = await getLocation();
+    set(state => ({ currentLoc: { lat, long } }))
+  },
   setUserPw: (pw) => set(state => ({ pw })),
   setUserPWCheck: (check) => set(state => ({ pwCheck: check })),
 }))
