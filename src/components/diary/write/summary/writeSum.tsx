7460ce292763c8940ac7e86003a514f145c24c3f
yusuShin <tlsdbtn0507@naver.com>
2024-10-04 23:23:38 +0900

    현재 위치 저장 실헹을 writeSum 컴포넌트에서 실행

--- src/components/diary/write/summary/writeSum.tsx
+++ src/components/diary/write/summary/writeSum.tsx
@@ -2,15 +2,28 @@ import { whichObjIsEmpty } from 'utils/util'
 import css from '../../../../css/write.module.css'
 import Selected from './selected'
 import { diaryStore } from 'store/diary/diaryStore'
+import { useEffect, useState } from 'react'
+import { userStore } from 'store/user/userStore'
+import { getRealWeather } from 'api/api'
 
 const WriteSum = () => {
 
   const { isDiaryWritten } = diaryStore(state => state);
+  const { currentLoc, setCurrentLoc } = userStore(state => state);
+  const [showSelected, setShowSelected] = useState(false);
+
+  useEffect(() => {
+    if (whichObjIsEmpty(isDiaryWritten) === 'done') {
+      setShowSelected(true);
+      Object.entries(currentLoc).every(e => e[1] === '') ?
+        setCurrentLoc() : getRealWeather(currentLoc);
+    }
+  }, [isDiaryWritten, currentLoc, setCurrentLoc]);
 
   return (
     <div className={css.writeSumWrap}>
       <h2>오늘 하루의</h2>
-      {whichObjIsEmpty(isDiaryWritten) === 'done' && <Selected />}
+      {showSelected && <Selected />}
     </div>
   )
 }
