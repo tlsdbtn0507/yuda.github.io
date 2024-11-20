aa6e970ae19be4cf929f183d49588b7da77e2f58
yusuShin <tlsdbtn0507@naver.com>
2024-11-20 23:44:45 +0900

    style(homePage.tsx) prettier 사용

--- src/router/homePage.tsx
+++ src/router/homePage.tsx
@@ -1,63 +1,61 @@
-import LastToday from '../components/lastToday'
-import MyDiaries from '../components/myDiaries'
-import Nav from '../components/nav/nav'
-import DayMaker from '../components/util/dayMaker'
-import Write from '../components/diary/write/write'
-import css from '../css/main.module.css'
+import LastToday from "../components/lastToday";
+import MyDiaries from "../components/myDiaries";
+import Nav from "../components/nav/nav";
+import DayMaker from "../components/util/dayMaker";
+import Write from "../components/diary/write/write";
+import css from "../css/main.module.css";
 
-import { useQuery } from '@tanstack/react-query'
-import { getDiaries } from '../api/diary/diaryApi'
-import { diaryStore } from '../store/diary/diaryStore'
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router'
-import { userStore } from 'store/user/userStore'
-import { isEmptyObj } from 'utils/util'
+import { useQuery } from "@tanstack/react-query";
+import { getDiaries } from "../api/diary/diaryApi";
+import { diaryStore } from "../store/diary/diaryStore";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { userStore } from "store/user/userStore";
+import { isEmptyObj } from "utils/util";
 
 const HomePage: React.FC = () => {
-
   const navigate = useNavigate();
 
   const { data, isError, error } = useQuery({
-    queryKey: ['diaries'],
+    queryKey: ["diaries"],
     queryFn: getDiaries,
   });
 
-  const { getDiaries: fetchingDiary, toggleWriteDairy, isWritingDairy } =
-    diaryStore(state => state);
+  const { getDiaries: fetchingDiary, toggleWriteDairy, isWritingDairy, } = diaryStore((state) => state);
 
-  const { setCurrentLoc, currentLoc } = userStore(state => state);
+  const { setCurrentLoc, currentLoc } = userStore((state) => state);
 
   const currentLocDiaries = !isEmptyObj(currentLoc) && <p>지금 위치에 있던 기록들</p>;
 
   useEffect(() => {
     if (isError) {
       alert(error.message);
-      navigate('/login');
+      navigate("/login");
       window.location.reload();
-    } else {
-      if (data) fetchingDiary(data);
+      return
     }
+    if (data) return fetchingDiary(data);
 
     setCurrentLoc();
-
   }, [isError, data]);
 
   return (
     <>
       <div className={`${css.total} ${isWritingDairy ? css.expand : css.home}`}>
-        {
-          isWritingDairy ? <Write /> :
-            <div className={css.wrapper}>
-              <DayMaker />
-              <LastToday />
-              <MyDiaries />
-              {currentLocDiaries}
-            </div>
-        }
+        {isWritingDairy ? (
+          <Write />
+        ) : (
+          <div className={css.wrapper}>
+            <DayMaker />
+            <LastToday />
+            <MyDiaries />
+            {currentLocDiaries}
+          </div>
+        )}
       </div>
       {!isWritingDairy && <Nav onDiaryClick={() => toggleWriteDairy(true)} />}
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
