9515b4e64e07fc88e45dafd7ebea486b7aa6626d
yusuShin <tlsdbtn0507@naver.com>
2024-11-23 21:03:57 +0900

    style(wirteSum.tsx) prettier 사용

--- src/components/diary/write/summary/writeSum.tsx
+++ src/components/diary/write/summary/writeSum.tsx
@@ -1,18 +1,17 @@
-import { whichObjIsEmpty } from 'utils/util'
-import css from '../../../../css/write.module.css'
-import Selected from './selected'
-import { diaryStore } from 'store/diary/diaryStore'
-import { useEffect, useState } from 'react'
-import { userStore } from 'store/user/userStore'
+import { whichObjIsEmpty } from "utils/util";
+import css from "../../../../css/write.module.css";
+import Selected from "./selected";
+import { diaryStore } from "store/diary/diaryStore";
+import { useEffect, useState } from "react";
+import { userStore } from "store/user/userStore";
 
 const WriteSum = () => {
-
-  const { isDiaryWritten } = diaryStore(state => state);
-  const { currentLoc, setCurrentLoc } = userStore(state => state);
+  const { isDiaryWritten } = diaryStore((state) => state);
+  const { currentLoc, setCurrentLoc } = userStore((state) => state);
   const [showSelected, setShowSelected] = useState(false);
 
   useEffect(() => {
-    if (whichObjIsEmpty(isDiaryWritten) === 'done') setShowSelected(true);
+    if (whichObjIsEmpty(isDiaryWritten) === "done") setShowSelected(true);
   }, [isDiaryWritten, currentLoc, setCurrentLoc]);
 
   return (
@@ -20,7 +19,7 @@ const WriteSum = () => {
       <h2>오늘 하루의</h2>
       {showSelected && <Selected />}
     </div>
-  )
-}
+  );
+};
 
-export default WriteSum
+export default WriteSum;
