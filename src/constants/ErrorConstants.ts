5ff145630c3df3771289956e9a7a7c425ad00f41
yusuShin <tlsdbtn0507@naver.com>
2024-11-25 23:30:04 +0900

    docx(ErrorConstants.ts) 예외처리 전용 상수파일 생성 - 에러 및 예외처리에 쓸 문자열 상수들을 담은 파일

---
+++ src/constants/ErrorConstants.ts
@@ -0,0 +1,9 @@
+const ERROR = {
+  LOGIN_FAIL: "로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요",
+  FETCH_DIARY: "다이러리 부르기 실패",
+  RENEW_TOKEN: "토큰 갱신 실패",
+  LOGOUT_FAIL: "로그아웃 실패",
+  LOGOUT_ALERT: "로그 아웃 오류",
+};
+
+export default ERROR;
