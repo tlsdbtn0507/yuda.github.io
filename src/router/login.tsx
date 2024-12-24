4c21527b5754e8df38e18b2a8cf05a968fcf087b
yusuShin <tlsdbtn0507@naver.com>
2024-12-24 18:17:35 +0900

    feat(login.tsx) 로그인 페이지에 로딩스핀 추가

--- src/router/login.tsx
+++ src/router/login.tsx
@@ -1,29 +1,41 @@
-import { useNavigate } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLock, faUser } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom'
-import { useRef } from 'react'
-import { useMutation } from '@tanstack/react-query'
-import { login } from '../api/users/usersApi'
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { login } from "../api/users/usersApi";
+import { handleAlertPerDevice } from "utils/util";
 
-import css from '../css/login.module.css'
-import UI from 'constants/uiConstants'
-import ERROR from 'constants/ErrorConstants'
-import APIS from 'constants/apiConstants'
-import { handleAlertPerDevice } from 'utils/util'
+import css from "../css/login.module.css";
+import UI from "constants/uiConstants";
+import ERROR from "constants/ErrorConstants";
+import APIS from "constants/apiConstants";
+import LoginBtnSpin from "components/login/loginBtnSpin";
 
-const { EMPTY_STRING, LOGIN: { HEAD, ID, PW, UI_LOGIN, GO_SIGN, SIGN } } = UI;
-const { ROUTES: { MAIN }, IS_USER_LOGINED_STR, IS_USER_LOGINED_TRUE } = APIS;
+const {
+  EMPTY_STRING,
+  LOGIN: { HEAD, ID, PW, GO_SIGN, SIGN },
+} = UI;
+const {
+  ROUTES: { MAIN },
+  IS_USER_LOGINED_STR,
+  IS_USER_LOGINED_TRUE,
+} = APIS;
 
 const Login = () => {
-
   const idRef = useRef<HTMLInputElement>(null);
   const pwRef = useRef<HTMLInputElement>(null);
+
+  const [showLoadingSpin, setShowLoadingSpin] = useState(false);
 
   const navigate = useNavigate();
 
   const { mutate } = useMutation({
     mutationFn: login,
+    onSettled() {
+      setShowLoadingSpin(false);
+    },
     onSuccess(data) {
       if (data) {
         localStorage.setItem(IS_USER_LOGINED_STR, `${IS_USER_LOGINED_TRUE}`);
@@ -43,34 +55,31 @@ const Login = () => {
     const id = idRef.current!.value;
     const pw = pwRef.current!.value;
 
-    mutate({ id, pw })
-  }
+    mutate({ id, pw });
+    setShowLoadingSpin(true);
+  };
 
   return (
     <div className={css.div}>
-      <form className={css.form} method='post' onSubmit={submitLogin}>
+      <form className={css.form} method="post" onSubmit={submitLogin}>
         <h1>{HEAD}</h1>
-        <label htmlFor='id' id='id' className={css.font}>
+        <label htmlFor="id" id="id" className={css.font}>
           <FontAwesomeIcon icon={faUser} />
           {ID}
         </label>
         <input ref={idRef} required type="text" name="id" id="id" />
-        <label htmlFor='pw' id='pw' className={css.font}>
+        <label htmlFor="pw" id="pw" className={css.font}>
           <FontAwesomeIcon icon={faLock} />
           {PW}
         </label>
         <input ref={pwRef} required type="password" name="pw" id="pw" />
-        <button type='submit' className={css.btn}>
-          {UI_LOGIN}
-        </button>
+        <LoginBtnSpin showLoadingSpin={showLoadingSpin} />
         <p className={css.sign}>
           {GO_SIGN}
-          <Link to={'/sign'}>
-            {SIGN}
-          </Link>
+          <Link to={"/sign"}>{SIGN}</Link>
         </p>
       </form>
     </div>
-  )
-}
+  );
+};
 export default Login;
