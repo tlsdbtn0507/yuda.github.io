0802cbeef6127f569d11f377359a0d66a59b7c2f
yusuShin <tlsdbtn0507@naver.com>
2024-12-25 22:37:26 +0900

    feat(sign.tsx) 회원가입 시 아이디 중복 체크 로딩스핀 추가

--- src/components/sign/idCheckBtn.tsx
+++ src/components/sign/idCheckBtn.tsx
@@ -3,12 +3,13 @@ import { BtnType } from '../../model/interfaces';
 
 import css from '../../css/sign.module.css'
 import UI from 'constants/uiConstants';
+import LoadingSpin from 'components/util/loadingSpin';
 
 const { CHECK_ID_DUPLE, STRING_NOT, CANT_USE, CAN_USE } = UI.IdCheckBtnTsx;
 
 const IdCheckBtn = (props: BtnType) => {
 
-  const { isIdVal } = props;
+  const { isIdVal, isSpinActivate } = props;
 
   const [isClicked, setIsClicked] = useState(false);
 
@@ -27,13 +28,20 @@ const IdCheckBtn = (props: BtnType) => {
     return btnContent;
   }
 
+  console.log(isSpinActivate);
+
 
   return (
-    <button className=
-      {makeBtnContent() === CHECK_ID_DUPLE || isIdVal ? css.checkBtn : css.checkBtnValid}
-      onClick={send}>
-      {makeBtnContent()}
-    </button>
+    <div className={css.idDupleSpinDiv}>
+      {/* {isSpinActivate && <LoadingSpin />} */}
+
+      <button className=
+        {makeBtnContent() === CHECK_ID_DUPLE || isIdVal ? css.checkBtn : css.checkBtnValid}
+        onClick={send}>
+        {makeBtnContent()}
+      </button>
+      <LoadingSpin />
+    </div>
   )
 }
 
