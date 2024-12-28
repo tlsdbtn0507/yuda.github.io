e9274929e5c119a14f0e5bc0373ddadf27eb63c7
yusuShin <tlsdbtn0507@naver.com>
2024-12-28 17:53:39 +0900

    feat(sign.tsx) 비밀번호 조건 문자열 위치 수정 - 기존은 input의 placeholder로 처리 했으나 가독성과 css가 깨져서 - 하단에 p태그를 추가해 비밀번호 조건을 안내함

--- src/router/sign.tsx
+++ src/router/sign.tsx
@@ -1,17 +1,22 @@
-import { Form } from 'react-router-dom'
-import { userStore } from '../store/user/userStore';
-import { useRef, useState } from 'react';
-import { checkIdDuple } from '../api/users/usersApi';
-import { useMutation } from '@tanstack/react-query';
-import { handleAlertPerDevice } from 'utils/util';
+import { Form } from "react-router-dom";
+import { userStore } from "../store/user/userStore";
+import { useRef, useState } from "react";
+import { checkIdDuple } from "../api/users/usersApi";
+import { useMutation } from "@tanstack/react-query";
+import { handleAlertPerDevice } from "utils/util";
 
-import PwDiv from '../components/sign/pwDiv';
-import IdCheckBtn from '../components/sign/idCheckBtn';
-import css from '../css/sign.module.css'
-import UI from 'constants/uiConstants';
-import APIS from 'constants/apiConstants';
+import PwDiv from "../components/sign/pwDiv";
+import IdCheckBtn from "../components/sign/idCheckBtn";
+import css from "../css/sign.module.css";
+import UI from "constants/uiConstants";
+import APIS from "constants/apiConstants";
 
-const { EMPTY_STRING, IdCheckBtnTsx: { STRING_NOT }, SignTsx } = UI;
+const {
+  EMPTY_STRING,
+  IdCheckBtnTsx: { STRING_NOT },
+  SignTsx,
+  PwDivTsx: { STRING_UNDER_SIX },
+} = UI;
 const {
   CHECK_ID,
   ASK_CHECK_ID_DUPLE,
@@ -19,34 +24,37 @@ const {
   H1_TITLE,
   INPUT_LABEL: { NAME, ID, PW, PW_CHECK },
   PLACE_HOLDER,
-  SIGN_DONE
+  SIGN_DONE,
 } = SignTsx;
 const { STRING_PWCHECK } = APIS;
 
 const Sign = () => {
-
   const [idChecked, setIdChecked] = useState<boolean | string>(STRING_NOT);
   const [idInput, setIdInput] = useState<string>(EMPTY_STRING);
   const [spinActivate, setSpinActivate] = useState<boolean>(false);
 
   const idInputRef = useRef<HTMLInputElement>(null);
 
-  const { pwCheck } = userStore(state => state);
+  const { pwCheck } = userStore((state) => state);
 
   const { mutate } = useMutation({
     mutationFn: checkIdDuple,
     onSettled: () => setSpinActivate(false),
-    onSuccess: (data) => setIdChecked(data)
+    onSuccess: (data) => setIdChecked(data),
   });
 
   const checkIsIdDuplicated = (e: React.FormEvent) => {
     e.preventDefault();
-    setSpinActivate(true)
+    setSpinActivate(true);
 
     const { value } = idInputRef.current as HTMLInputElement;
     const KOREAN_REGEX = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
 
-    if (value === EMPTY_STRING || KOREAN_REGEX.test(value) || value.length < 4) {
+    if (
+      value === EMPTY_STRING ||
+      KOREAN_REGEX.test(value) ||
+      value.length < 4
+    ) {
       idInputRef.current!.value = EMPTY_STRING;
       idInputRef.current?.focus();
       setIdChecked(STRING_NOT);
@@ -61,8 +69,8 @@ const Sign = () => {
     const target = idInputRef.current?.value as string;
 
     //먼저 사용불가된 id를 또 입력할 때를 대비한 코드
-    if (idInput !== target) setIdChecked(STRING_NOT)
-  }
+    if (idInput !== target) setIdChecked(STRING_NOT);
+  };
 
   const permitSub = (e: React.FormEvent) => {
     if (!idChecked) {
@@ -72,42 +80,46 @@ const Sign = () => {
     }
     if (!pwCheck) {
       handleAlertPerDevice(ASK_CHECK_PW_DUPLE);
-      document.getElementById(STRING_PWCHECK)?.focus()
+      document.getElementById(STRING_PWCHECK)?.focus();
       return e.preventDefault();
     }
-    if (idChecked && pwCheck) return
-  }
+    if (idChecked && pwCheck) return;
+  };
 
   return (
     <div className={css.total}>
-      <Form className={css.div} method='post'>
+      <Form className={css.div} method="post">
         <h1>{H1_TITLE}</h1>
-        <label htmlFor="userName">
-          {NAME}
-        </label>
-        <input required type="userName" id='name' name='name' />
+        <label htmlFor="userName">{NAME}</label>
+        <input required type="userName" id="name" name="name" />
         <label htmlFor="userId">
           {ID}
-          <IdCheckBtn onClick={checkIsIdDuplicated} isIdVal={idChecked} isSpinActivate={spinActivate} />
+          <IdCheckBtn
+            onClick={checkIsIdDuplicated}
+            isIdVal={idChecked}
+            isSpinActivate={spinActivate}
+          />
         </label>
         <input
-          required type="userId" id='userId' name='userId' ref={idInputRef}
+          required
+          type="userId"
+          id="userId"
+          name="userId"
+          ref={idInputRef}
           onChange={checkId}
-          placeholder={PLACE_HOLDER} />
-        <label htmlFor="pw">
-          {PW}
-        </label>
-        <PwDiv type='pw' />
-        <label htmlFor="pwCheck">
-          {PW_CHECK}
-        </label>
-        <PwDiv type='pwCheck' />
-        <button type='submit' onClick={permitSub} className={css.sendBtn}>
+          placeholder={PLACE_HOLDER}
+        />
+        <label htmlFor="pw">{PW}</label>
+        <PwDiv type="pw" />
+        <p className={css.pwConditionString}>{STRING_UNDER_SIX}</p>
+        <label htmlFor="pwCheck">{PW_CHECK}</label>
+        <PwDiv type="pwCheck" />
+        <button type="submit" onClick={permitSub} className={css.sendBtn}>
           {SIGN_DONE}
         </button>
       </Form>
     </div>
-  )
-}
+  );
+};
 
-export default Sign
+export default Sign;
