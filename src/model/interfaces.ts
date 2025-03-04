6845f9fc789685638ca3e60ed8e0f78455450109
yusuShin <tlsdbtn0507@naver.com>
2025-03-05 00:48:14 +0900

    [Feat] 서버에서 온 다이어리 데이터 타입 추가

--- src/model/interfaces.ts
+++ src/model/interfaces.ts
@@ -82,6 +82,14 @@ export interface DiaryToSendToSurver extends Required<IsDiaryWritten> {
   dayOfWeek: Days;
 }
 
+export interface DiaryCameFromServer extends DiaryToSendToSurver {
+  id: number;
+  humidity:string;
+  rainAmount:string;
+  rainCod:string;
+  temp:string;
+}
+
 /* eslint-disable no-unused-vars */
 export const enum WriteDiaryEnum {
   Feeling = "feeling",
