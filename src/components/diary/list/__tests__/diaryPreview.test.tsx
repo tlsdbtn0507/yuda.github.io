c4f19169f473a2b70f1ac734166b54437ae9340e
yusuShin <tlsdbtn0507@naver.com>
2025-04-02 22:59:02 +0900

    [Test] PreviewWrapper의 버튼클릭 테스트 추가

--- src/components/diary/list/__tests__/diaryPreview.test.tsx
+++ src/components/diary/list/__tests__/diaryPreview.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
 
 import React from "react";
 import DiaryPreview from "../diaryPreview";
 import PreviewWrapper from "../previewWrapper";
-
 import TEST from "../../../../constants/testConstants";
 
 const { DUMMI_DIARY_DATA } = TEST;
@@ -12,31 +12,66 @@ describe("DiaryPreview", () => {
   test("DiaryPreview컴포넌트가 잘 나올까요?", () => {
     
     render(<DiaryPreview diaryDetails={DUMMI_DIARY_DATA} />);
-    const isComponentHasClass = screen.getByTestId("diaryPreview");
+    
+    const fullText = `${DUMMI_DIARY_DATA.diaryDate.replaceAll("-", " . ")} ${String(DUMMI_DIARY_DATA.dayOfWeek)}`;
+    const isFullTextExist = screen.getByText(fullText,{exact: false});
 
-    const dateOfDiary = screen.getByText(/\d{4} \. \d{2} \. \d{2}/);
-
-    expect(isComponentHasClass).toBeInTheDocument();
-    expect(dateOfDiary).toBeInTheDocument();
+    expect(isFullTextExist).toBeInTheDocument();
 
   });
-  test("DiaryPreview컴포넌트가 자식 컴포넌트를 잘 불러올까요?", () => {
-    render(<DiaryPreview diaryDetails={DUMMI_DIARY_DATA} />);
-    const stringTobeExpected = screen.getByTestId("diaryPreviewWrapper");
-    
-    expect(stringTobeExpected).toBeInTheDocument();
-  })
 
   test("PreviewWrapper가 버튼들과 내용을 잘 불러올까요?", () => {
-    render(<PreviewWrapper />);
+    render(<PreviewWrapper diaryPreviewContent={DUMMI_DIARY_DATA} />);
 
     const sideBtns = screen.getAllByTestId("sideBtns");
     expect(sideBtns).toHaveLength(2);
 
-    const previewWrapperContent = screen.getByText("완전 더웠던 하루", { exact: false });
+    const previewWrapperContent = screen.getByText("완전 행복했던 하루", { exact: true });
     expect(previewWrapperContent).toBeInTheDocument();
 
     const carouselBtns = screen.getAllByTestId("carouselBtns");
     expect(carouselBtns).toHaveLength(4);
   })
+  
+})
+
+describe("PreviewWrapper의 버튼이 제대로 작동할까요?",()=> {
+
+  const renderAndClickRight = (times: number, isRight: boolean) => {
+    render(<PreviewWrapper diaryPreviewContent={DUMMI_DIARY_DATA} />);
+    const sideBtns = screen.getAllByTestId("sideBtns");
+    for (let i = 0; i < times; i++) {
+      fireEvent.click(isRight ? sideBtns[1] : sideBtns[0]);
+    }
+  };
+
+  test("우측으로 1번", () => {
+    renderAndClickRight(1,true);
+    expect(screen.getByText("너무 화창했던 하루")).toBeInTheDocument();
+  });
+
+  test("우측으로 2번", () => {
+    renderAndClickRight(2,true);
+    expect(screen.getByText("테스트")).toBeInTheDocument();
+  });
+
+  test("우측으로 3번", () => {
+    renderAndClickRight(3,true);
+    expect(screen.getByText("완전 행복했던 하루")).toBeInTheDocument();
+  });
+
+  test("좌측으로 1번", () => {
+    renderAndClickRight(1,false);
+    expect(screen.getByText("테스트")).toBeInTheDocument();
+  });
+
+  test("좌측으로 2번", () => {
+    renderAndClickRight(2,false);
+    expect(screen.getByText("너무 화창했던 하루")).toBeInTheDocument();
+  });
+
+  test("좌측으로 3번", () => {
+    renderAndClickRight(3,false);
+    expect(screen.getByText("완전 행복했던 하루")).toBeInTheDocument();
+  });
 })
