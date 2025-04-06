3aaf34a1cfb64088d224fa53995bced865abc6c8
yusuShin <tlsdbtn0507@naver.com>
2025-04-06 23:52:15 +0900

    [Feat] PreviewWrapper의 오른쪽sideBtn 클릭시 애니메이션 추가

--- src/components/diary/list/previewWrapper.tsx
+++ src/components/diary/list/previewWrapper.tsx
@@ -1,30 +1,69 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { DiaryCameFromServer } from "model/interfaces";
+import { previewContentArrayMaker } from "../../../utils/util";
 
-import React from "react";
+import React, { act, useState } from "react";
 import css from "../../../css/preview.module.css";
 import CarouselBtns from "../../util/caroselBtn";
+import SpecificWeather from "./specificWeather";
 
-const PreviewWrapper = (props: any) => {
+interface PreviewWrapperProps {
+  diaryPreviewContent: DiaryCameFromServer
+}
 
-  const content = <p className={css.contentText}>완전 더웠던 하루</p>;
+const PreviewWrapper: React.FC<PreviewWrapperProps> = ({ diaryPreviewContent }) => {
+  
+  const previewContents = previewContentArrayMaker(diaryPreviewContent);
+  const currentContentArray = Object.values(previewContents);
+  const [currentContentIndex, setCurrentContentIndex] = useState(0);
+  const [animationDirection, setAnimationDirection] = useState(`${css.contentText}`);
+
+  const content = currentContentIndex === 2 ?
+    <SpecificWeather specificWeather={previewContents.specificWeatherInDiary} /> :
+    <p className={animationDirection}>{currentContentArray[currentContentIndex] as string}</p>
+
 
   const carouselBtns =
     <div className={css.carouselBtnsWrapper}>
-      {[0, 1, 2, 3].map(e => <CarouselBtns id={`${e}`} key={e} />)}
-    </div>
+      {[0, 1, 2].map(e => <CarouselBtns id={`${e}`} key={e} isToggleBtn={ currentContentIndex === e } />)}
+    </div>;
+  
+
+  const moveContentHandler = (direction: number) => {
+    if (currentContentIndex + direction < 0) {
+      return setCurrentContentIndex(2);
+    }
+    if (currentContentIndex + direction > 2) {
+      return setCurrentContentIndex(0);
+    }
+    setCurrentContentIndex(currentContentIndex + direction);
+  }
+
+  const moveToRight = () => {
+    setAnimationDirection(`${css["constent-slide-out-left"]}`);
+    setTimeout(() => {
+      moveContentHandler(1);
+      setAnimationDirection(`${css["constent-slide-in-right"]}`);
+    }, 400);
+  };
+
+  const moveToLeft = () => {
+
+  }
 
   return (
-    <div {...props} className={css.wrapper}>
+    <div className={css.wrapper}>
       <div className={css.contentWrapper}>
-        <button data-testid="sideBtns" className={css.sideBtns}>
+        <button data-testid="sideBtns" className={css.sideBtns} onClick={moveToLeft}>
           <FontAwesomeIcon icon={faAngleLeft}/>
         </button>
         <div className={css.content}>
-          {content}
+          { content }
           {carouselBtns}
         </div>
-        <button data-testid="sideBtns" className={css.sideBtns}>
+        {/* <button data-testid="sideBtns" className={css.sideBtns} onClick={() => activeClickHandler(1)}> */}
+        <button data-testid="sideBtns" className={css.sideBtns} onClick={moveToRight}>
           <FontAwesomeIcon icon={faAngleRight}/>
         </button>
       </div>
